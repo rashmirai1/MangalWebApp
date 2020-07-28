8e434bdd00a1b7dafc3625fe71cbbf713f2d1dea
Nitesh1-91 <niteshtiwari486@gmail.com>
2020-07-28 20:47:04 +0530

    pincode

---
+++ MangalWeb.Repository/Repository/PincodeRepository.cs
@@ -0,0 +1,96 @@
+﻿
+using MangalWeb.Model.Entity;
+using MangalWeb.Model.Masters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangalWeb.Repository.Repository
+{
+    public class PincodeRepository
+    {
+        MangalDBNewEntities _context = new MangalDBNewEntities();
+
+        public List<Mst_PinCode> GetAllPincodeMasters()
+        {
+            var list = _context.Mst_PinCode.ToList();
+            return list;
+        }
+
+        public List<tblCityMaster> GetCityMasterList()
+        {
+            var list = _context.tblCityMasters.ToList();
+            return list;
+        }
+
+        public List<tblZonemaster> GetZoneMasterList()
+        {
+            var list = _context.tblZonemasters.ToList();
+            return list;
+        }
+
+        public string GetStateName(int id)
+        {
+            var stateid =_context.tblCityMasters.Where(x => x.CityID ==id).Select(x => x.StateID).FirstOrDefault();
+            var statename = _context.tblStateMasters.Where(x => x.StateID == stateid).Select(x => x.StateName).FirstOrDefault();
+            return statename;
+        }
+
+        public Mst_PinCode GetPincodeById(int id)
+        {
+            var pincode = _context.Mst_PinCode.Where(x => x.Pc_Id == id).FirstOrDefault();
+            return pincode;
+        }
+
+        public void DeleteRecord(int id)
+        {
+            var deleterecord = _context.Mst_PinCode.Where(x => x.Pc_Id == id).FirstOrDefault();
+            if (deleterecord != null)
+            {
+                _context.Mst_PinCode.Remove(deleterecord);
+                _context.SaveChanges();
+            }
+        }
+
+        public void SaveUpdateRecord(PincodeViewModel model)
+        {
+            Mst_PinCode tblpincode = new Mst_PinCode();
+            if (model.ID <= 0)
+            {
+                tblpincode.Pc_RecordCreated = DateTime.Now;
+                tblpincode.Pc_RecordCreatedBy = model.CreatedBy;
+                _context.Mst_PinCode.Add(tblpincode); ;
+            }
+            else
+            {
+                tblpincode = _context.Mst_PinCode.Where(x => x.Pc_Id == model.ID).FirstOrDefault();
+            }
+            tblpincode.Pc_Desc = model.Pincode;
+            tblpincode.Pc_AreaName = model.AreaName;
+            tblpincode.Pc_CityId = model.CityId;
+            tblpincode.Pc_ZoneId = model.ZoneId;
+            tblpincode.Pc_RecordUpdated = DateTime.Now;
+            tblpincode.Pc_RecordUpdatedBy = model.UpdatedBy;
+            _context.SaveChanges();
+        }
+
+        public string CheckPincodeExists(string Name)
+        {
+            var pincode = _context.Mst_PinCode.Where(x => x.Pc_AreaName == Name).Select(x => x.Pc_AreaName).FirstOrDefault();
+            return pincode;
+        }
+
+        public PincodeViewModel SetRecordinEdit(Mst_PinCode tblpincode)
+        {
+            PincodeViewModel pincode = new PincodeViewModel();
+            pincode.ID = tblpincode.Pc_Id;
+            pincode.Pincode = tblpincode.Pc_Desc;
+            pincode.AreaName = tblpincode.Pc_AreaName;
+            pincode.CityId = tblpincode.Pc_CityId;
+            pincode.ZoneId = tblpincode.Pc_ZoneId;
+            return pincode;
+        }
+    }
+}
---
+++ MangalWeb/Controllers/PincodeController.cs
@@ -0,0 +1,109 @@
+﻿using MangalWeb.Model.Masters;
+using MangalWeb.Service.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MangalWeb.Controllers
+{
+    public class PincodeController : BaseController
+    {
+        PincodeService _pincodeService = new PincodeService();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult CreateEdit(PincodeViewModel pincodeViewModel)
+        {
+            pincodeViewModel.CreatedBy = Convert.ToInt32(Session["UserLoginId"]);
+            pincodeViewModel.UpdatedBy = Convert.ToInt32(Session["UserLoginId"]);
+            try
+            {
+                if (pincodeViewModel.ID <= 0)
+                {
+                    var data = _pincodeService.CheckPinAreaExists(pincodeViewModel.AreaName);
+                    if (data != null)
+                    {
+                        ModelState.AddModelError("AreaName", "Area Name Already Exists");
+                        return Json(pincodeViewModel);
+                    }
+                }
+                _pincodeService.SaveUpdateRecord(pincodeViewModel);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Json(pincodeViewModel);
+        }
+
+        public ActionResult GetPincodeById(int ID)
+        {
+            string operation = Session["Operation"].ToString();
+            ButtonVisiblity(operation);
+            var tblpincode = _pincodeService.GetPincodeById(ID);
+            var model = new PincodeViewModel();
+            if (tblpincode != null)
+            {
+                model = _pincodeService.SetDataOnEdit(tblpincode);
+            }
+            model.operation = operation;
+            ViewBag.CityList = new SelectList(_pincodeService.GetCityMasterList(), "CityId", "CityName");
+            ViewBag.ZoneList = new SelectList(_pincodeService.GetZoneMasterList(), "ZoneId", "Zone");
+            var statename = _pincodeService.GetStateName(model.CityId);
+            model.StateName = statename;
+            return View("Pincode", model);
+        }
+
+        // GETDelete/5
+        public ActionResult Delete(int id)
+        {
+            string data = "";
+            _pincodeService.DeleteRecord(id);
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult doesAreaNameExist(string AreaName)
+        {
+            var data = _pincodeService.CheckPinAreaExists(AreaName);
+            var result = "";
+            //Check if city name already exists
+            if (data != null)
+            {
+                if (AreaName.ToLower() == data.ToLower().ToString())
+                {
+                    result = "Area Already Exists";
+                }
+                else
+                {
+                    result = "";
+                }
+            }
+            return Json(result);
+        }
+
+        public ActionResult Pincode()
+        {
+            ButtonVisiblity("Index");
+            var model = new PincodeViewModel();
+            ViewBag.CityList = new SelectList(_pincodeService.GetCityMasterList(), "CityId", "CityName");
+            ViewBag.ZoneList = new SelectList(_pincodeService.GetZoneMasterList(), "ZoneId", "Zone");
+            return View(model);
+        }
+
+        public ActionResult GetPincodeTable(string Operation)
+        {
+            Session["Operation"] = Operation;
+            //ButtonVisiblity(Operation);
+            return PartialView("_PincodeList", _pincodeService.GetAllPincodeMaster());
+        }
+
+        public JsonResult GetState(int id)
+        {
+            var statename = _pincodeService.GetStateName(id);
+            return Json(statename, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
