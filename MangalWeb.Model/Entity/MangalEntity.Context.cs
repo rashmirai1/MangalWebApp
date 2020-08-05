b3560d19b4e4962ccbd99267622039d10567b77b
Rashmi Rai <Rashmi Rai@RASHMI-WORKSTAT>
2020-08-05 19:33:33 +0530

    resolved merge errors

--- MangalWeb.Model/Entity/MangalEntity.Context.cs
+++ MangalWeb.Model/Entity/MangalEntity.Context.cs
@@ -110,7 +110,6 @@ namespace MangalWeb.Model.Entity
         public virtual DbSet<Mst_ProductRateDetails> Mst_ProductRateDetails { get; set; }
         public virtual DbSet<tblItemMaster> tblItemMasters { get; set; }
         public virtual DbSet<TSchemeMaster_BasicDetails> TSchemeMaster_BasicDetails { get; set; }
-        public virtual DbSet<tbl_UserCategory> tbl_UserCategory { get; set; }
         public virtual DbSet<TGLKYC_BasicDetails> TGLKYC_BasicDetails { get; set; }
         public virtual DbSet<tbl_UserCategory> tbl_UserCategory { get; set; }
         public virtual DbSet<Mst_BranchType> Mst_BranchType { get; set; }
