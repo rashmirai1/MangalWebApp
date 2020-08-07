e45e9fc81deb57a02bf46a0d0aa1ee23537925e5
Nitesh1-91 <niteshtiwari486@gmail.com>
2020-08-07 12:25:01 +0530

    changesindocument

---
+++ MangalWeb/Controllers/DocumentVerificationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MangalWeb.Controllers
+{
+    public class DocumentVerificationController : Controller
+    {
+        // GET: DocumentVerification
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
