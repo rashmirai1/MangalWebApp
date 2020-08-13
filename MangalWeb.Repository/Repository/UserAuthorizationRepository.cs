96045a8c073b28dd663826d40c04c02310d73c12
Nitesh1-91 <niteshtiwari486@gmail.com>
2020-08-13 13:31:44 +0530

    authentication

---
+++ MangalWeb.Repository/Repository/UserAuthorizationRepository.cs
@@ -0,0 +1,58 @@
+﻿using MangalWeb.Model.Entity;
+using MangalWeb.Model.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangalWeb.Repository.Repository
+{
+   public class UserAuthorizationRepository
+    {
+        MangalDBNewEntities _context = new MangalDBNewEntities();
+
+        public List<UserAuthorizationForms> GetFormDetails_FormIdWise(int formid, int UserId)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("FormId", formid);
+            ht.Add("UserId", UserId);
+            var lst = _context.Database.SqlQuery<UserAuthorizationForms>("T_GetFormDetails_FormIdWise @FormId,@UserId",
+                new SqlParameter("FormId", formid),new SqlParameter("FormId",UserId)).ToList();
+            return lst;
+        }
+
+        public List<tbl_UserCategory> GetUserCategory()
+        {
+            return _context.tbl_UserCategory.ToList();
+        }
+        #region GetUser
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserCategoryId"></param>
+        /// <param name="BranchID"></param>
+        /// <returns></returns>
+        public List<UserDetail> GetUser(int UserCategoryId)
+        {
+            return _context.UserDetails.Where(x => x.UserTypeID == UserCategoryId).ToList();
+        }
+        #endregion
+
+        #region GetMenuListUserCategoryWise
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserCategoryId"></param>
+        /// <returns></returns>
+        public List<Menu> GetMenuList()
+        {
+
+            return _context.Menus.Where(x => x.ParentId == 0).ToList();
+        }
+        #endregion
+    }
+}
---
+++ MangalWeb.Service/Service/UserAuthorizationService.cs
@@ -0,0 +1,37 @@
+﻿using MangalWeb.Model.Entity;
+using MangalWeb.Model.Utilities;
+using MangalWeb.Repository.Repository;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangalWeb.Service.Service
+{
+    public class UserAuthorizationService
+    {
+        UserAuthorizationRepository _userAuthorizationRepository = new UserAuthorizationRepository();
+
+        public List<UserAuthorizationForms> GetFormDetails_FormIdWise(int formid, int UserId)
+        {
+            return _userAuthorizationRepository.GetFormDetails_FormIdWise(formid, UserId);
+        }
+
+        public List<tbl_UserCategory> GetUserCategory()
+        {
+            return _userAuthorizationRepository.GetUserCategory();
+        }
+
+        public List<UserDetail> GetUser(int catid)
+        {
+            return _userAuthorizationRepository.GetUser(catid);
+        }
+
+        public List<Menu> GetMenuList()
+        {
+            return _userAuthorizationRepository.GetMenuList();
+        }
+    }
+}
